4983aa585ec1541941bb8d4013a510c87c4cdfd1
woody <sarawut.core@gmail.com>
2024-08-26 17:48:22 +0700

    Full code flutter_infinite_list

---
+++ lib/posts/view/posts_page.dart
@@ -0,0 +1,22 @@
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/posts/bloc/post_bloc.dart';
+import 'package:http/http.dart' as http;
+
+import 'view.dart';
+
+class PostsPage extends StatelessWidget {
+  const PostsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocProvider(
+        create: (_) => PostBloc(httpClient: http.Client())..add(PostFetched()),
+        child: const PostsList(),
+      ),
+    );
+  }
+}
