cf1a8ef9358bd4c46675d12aee8bbf71a5a30461
woody <sarawut.core@gmail.com>
2024-08-26 18:14:50 +0700

    Fixed bug

--- lib/posts/view/posts_page.dart
+++ lib/posts/view/posts_page.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_infinite_list/posts/bloc/post_bloc.dart';
